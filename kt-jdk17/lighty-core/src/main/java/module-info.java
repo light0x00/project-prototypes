560ba3c1808e9eb4255e6dd696f6a1a01a8a644a
light0x00 <light0x00@163.com>
2024-07-28 14:09:24 +0800

    initial commit

---
+++ kt-jdk17/lighty-core/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * @author light0x00
+ * @since 2023/8/9
+ */
+module lighty.core {
+    requires kotlin.stdlib;
+    requires static jsr305;
+    requires org.slf4j;
+    requires static lombok;
+
+//    exports io.github.light0x00.lighty.core.eventloop;
+//    exports io.github.light0x00.lighty.core.buffer;
+//    exports io.github.light0x00.lighty.core.handler;
+//    exports io.github.light0x00.lighty.core.concurrent;
+//    exports io.github.light0x00.lighty.core.facade;
+}
